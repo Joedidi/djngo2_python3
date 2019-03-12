498ca4a777fc9b5d27c114a7ed21a616fdf20acd
jusk? <dreampeoplexyh@qq.com>
2019-03-12 20:15:44 +0800

    安装xadmin，完成xadmin的全部安装过程，并成功进入后台管理界面，接下来就是设计数据库结构以及后台管理系统

--- djngo2_python3/djngo2_python3/settings.py
+++ djngo2_python3/djngo2_python3/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'xadmin'
+    'xadmin',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
