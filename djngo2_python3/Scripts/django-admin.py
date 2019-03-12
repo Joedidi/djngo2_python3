f371d60503a140641988974fab09b8caf53a2cd9
jusk? <dreampeoplexyh@qq.com>
2019-03-12 20:33:01 +0800

    完成用户数据表的设计

---
+++ djngo2_python3/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\djngo2_python3\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- djngo2_python3/djngo2_python3/settings.py
+++ djngo2_python3/djngo2_python3/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#重载系统的用户，让UserProfile生效
+AUTH_USER_MODEL = 'users.UserProfile'
 
 # Application definition
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'xadmin',
     'crispy_forms',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -112,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hangs'
 
 TIME_ZONE = 'UTC'
 
@@ -127,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# 让用户上传的媒体文件放在这
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
