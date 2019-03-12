59d98c401a4dad82370947e8b9a41e52e2211fe9
jusk? <dreampeoplexyh@qq.com>
2019-03-12 22:40:31 +0800

    完成user_operation用户操作的数据表的设计并提交数据库中

---
+++ djngo2_python3/djngo2_python3/apps/user_operation/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from datetime import datetime
+from  goods.models import Goods
+
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+# Create your models here.
+
+class UserFav(models.Model):
+    """
+    用户收藏操作
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="用户")
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE, verbose_name="商品", help_text="商品id")
+    add_time = models.DateTimeField("添加时间", default=datetime.now)
+
+    class Meta:
+        verbose_name = "用户收藏"
+        verbose_name_plural = verbose_name
+        unique_together = ("user", "goods")
+
+    def __str__(self):
+        return self.user.username
+
+
+class UserAddress(models.Model):
+    """
+    用户收货地址
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="用户")
+    province = models.CharField("省份", max_length=100, default="")
+    city = models.CharField("城市", max_length=100, default="")
+    district = models.CharField("区域", max_length=100, default="")
+    address = models.CharField("详细地址", max_length=100, default="")
+    signer_name = models.CharField("签收人", max_length=100, default="")
+    signer_mobile = models.CharField("电话", max_length=11, default="")
+    add_time = models.DateTimeField("添加时间", default=datetime.now)
+
+    class Meta:
+        verbose_name = "收货地址"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.address
+
+class UserLeavingMessage(models.Model):
+    """
+    用户留言
+    """
+
+    MESSAGE_CHICES = (
+        (1, "留言"),
+        (2, "投诉"),
+        (3, "询问"),
+        (4, "售后"),
+        (5, "求购"),
+
+    )
+
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="用户")
+    message_type = models.BigIntegerField(default=1, choices=MESSAGE_CHICES, verbose_name="留言类型", help_text=u"留言类型:1(留言), 2(投诉), 3(询问), 4(售后), 5(求购)")
+    subject = models.CharField("主题", max_length=100, default="")
+    message = models.TextField("留言内同", default="", help_text="留言内容")
+    file = models.FileField(upload_to="message/images/", verbose_name="上传的文件", help_text="上传的文件")
+
+    add_time = models.DateTimeField("添加时间", default=datetime.now)
+
+    class Meta:
+        verbose_name="用户留言"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.subject
+
+
--- djngo2_python3/djngo2_python3/settings.py
+++ djngo2_python3/djngo2_python3/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'users',
     'goods',
     'trade',
+    'user_operation'
 ]
 
 MIDDLEWARE = [
