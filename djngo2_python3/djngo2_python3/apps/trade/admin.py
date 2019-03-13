d956fa64dcad0be7a48617fffe1abd368c850002
jusk? <dreampeoplexyh@qq.com>
2019-03-13 15:58:58 +0800

    修复订单消息bug

--- djngo2_python3/djngo2_python3/apps/trade/admin.py
+++ djngo2_python3/djngo2_python3/apps/trade/admin.py
@@ -7,12 +7,12 @@ from .models import ShoppingCart, OrderInfo, OrderGoods
 
 
 class ShopingCartAdmin(object):
-    list_display = ["user", "goods", "nums",]
+    list_display = ["user", "goods", "nums"]
 
 
 class OrderInfoAdmin(object):
     list_display = ["user", "order_sn", "trade_no", "pay_status", "post_script",
-                    "order_mount", "pay_time", "add_time"]
+                    "pay_time", "add_time"]
 
     class OrderGoodsInline(object):
         model = OrderGoods
--- djngo2_python3/djngo2_python3/urls.py
+++ djngo2_python3/djngo2_python3/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import xadmin
+from django.views.static import serve
+from djngo2_python3.settings import MEDIA_ROOT
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('ueditor/', include('DjangoUeditor.urls')),
+    #文件
+    path('media/<path:path>', serve, {'document_root': MEDIA_ROOT}),
 ]
