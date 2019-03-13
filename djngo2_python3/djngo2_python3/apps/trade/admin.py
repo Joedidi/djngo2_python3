5befc31c1b63a97968ca3470106d6aea24deee10
jusk? <dreampeoplexyh@qq.com>
2019-03-13 13:19:48 +0800

    完成trade 商品后台注册，并提交数据库，接下来设计user_operation, 后台交易的注册

--- djngo2_python3/djngo2_python3/apps/trade/admin.py
+++ djngo2_python3/djngo2_python3/apps/trade/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+import xadmin
+from .models import ShoppingCart, OrderInfo, OrderGoods
+
+
+class ShopingCartAdmin(object):
+    list_display = ["user", "goods", "nums",]
+
+
+class OrderInfoAdmin(object):
+    list_display = ["user", "order_sn", "trade_no", "pay_status", "post_script",
+                    "order_mount", "pay_time", "add_time"]
+
+    class OrderGoodsInline(object):
+        model = OrderGoods
+        exclude = ["add_time", ]
+        extra = 1
+        style = 'tab'
+
+    inlines = [OrderGoodsInline, ]
+
+xadmin.site.register(ShoppingCart, ShopingCartAdmin)
+xadmin.site.register(OrderInfo, OrderInfoAdmin)
