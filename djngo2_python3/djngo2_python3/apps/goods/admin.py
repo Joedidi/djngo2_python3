f6285a003034c0be546bef6a6df8b0e409083c9b
jusk? <dreampeoplexyh@qq.com>
2019-03-13 10:37:42 +0800

    完成goods 商品后台注册，并提交数据库，接下来设计trade, 后台交易的注册

--- djngo2_python3/djngo2_python3/apps/goods/admin.py
+++ djngo2_python3/djngo2_python3/apps/goods/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
 
 # Register your models here.
+
+import xadmin
+from .models import Goods, GoodsCategory, GoodsImage, GoodsCategoryBrand, Banner, HotSearchWords
+from .models import IndexAd
+
+
+class GoodsAdmin(object):
+    # 显示的列
+    list_display = ["name", "clicks_num", "sold_num", "fav_num", "goods_num", "market_price",
+                    "shop", "goods_brief", "goods_desc", "is_new", "is_hot", "add_time"]
+
+    # 可以搜索的字段
+    search_fields = ['name',]
+    # list_editable = ["is_hot",]
+    # 过滤器
+    list_filter = ["name", "click_num", "sold_num", "fav_num", "goods_num", "market_price",
+                   "shop_price", "is_new", "is_hot", "add_time", "category_name"]
+
+    # 富文本编辑器
+    style_fields = {"doods_desc": "ueditor"}
+
+    # 在添加商品的时候可以添加商品图片
+    class GoodsImagesInline(object):
+        model = GoodsImage
+        exclude = ["add_time"]
+        extra = 1
+        style = 'tab'
+
+    inlines = [GoodsImagesInline]
+
+class GoodsCategoryAdmin(object):
+    list_display = ["name", "category_type", "parent_category", "add_time"]
+    list_filter = ["category", "parent_category", "name"]
+    search_fields = ["name"]
+
+
+class GoodsBrandAdmin(object):
+    list_display = ["category", "image", "name", "desc"]
+
+    def get_context(self):
+        context = super(GoodsBrandAdmin, self).get_context()
+        if 'form' in context:
+            context['form'].fields['category'].queryset = GoodsCategory.objects.filter(category_type=1)
+        return context
+
+
+class BannerGoodsAdmin(object):
+    list_display = ["goods", "image", "index"]
+
+class HotSearchAdmin(object):
+    list_display = ["keywords", "index", "add_time"]
+
+class IndexAdmin(object):
+    list_display = ["category", "goods"]
+
+xadmin.site.register(Goods, GoodsAdmin)
+xadmin.site.register(GoodsCategory, GoodsCategoryAdmin)
+xadmin.site.register(Banner, BannerGoodsAdmin)
+xadmin.site.register(GoodsCategoryBrand, GoodsBrandAdmin)
+
+
+xadmin.site.register(HotSearchWords, HotSearchAdmin)
+xadmin.site.register(IndexAd, IndexAdmin)
--- djngo2_python3/djngo2_python3/apps/goods/models.py
+++ djngo2_python3/djngo2_python3/apps/goods/models.py
@@ -33,6 +33,23 @@ class GoodsCategory(models.Model):
     def __str__(self):
         return self.name
 
+class GoodsCategoryBrand(models.Model):
+    """
+    某一大类下的宣传商标
+    """
+    category = models.ForeignKey(GoodsCategory, on_delete=models.CASCADE, related_name='brands', null=True, blank=True, verbose_name="商品类目")
+    name = models.CharField("品牌名",default="", max_length=30,help_text="品牌名")
+    desc = models.TextField("品牌描述",default="", max_length=200,help_text="品牌描述")
+    image = models.ImageField(max_length=200, upload_to="brands/")
+    add_time = models.DateTimeField("添加时间",default=datetime.now)
+
+    class Meta:
+        verbose_name = "宣传品牌"
+        verbose_name_plural = verbose_name
+        db_table = "goods_goodsbrand"
+
+    def __str__(self):
+        return self.name
 
 
 class Goods(models.Model):
@@ -103,6 +120,20 @@ class Banner(models.Model):
     def __str__(self):
         return self.goods.name
 
+
+class IndexAd(models.Model):
+    """
+    商品广告
+    """
+    category = models.ForeignKey(GoodsCategory, on_delete=models.CASCADE, related_name='category',verbose_name="商品类目")
+    goods =models.ForeignKey(Goods, on_delete=models.CASCADE, related_name='goods')
+
+    class Meta:
+        verbose_name = '首页广告'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.goods.name
 
 class HotSearchWords(models.Model):
     """
